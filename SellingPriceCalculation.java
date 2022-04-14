5af0e783f6f697f5e83e2ba56e780a0806044e4d
santhosh039 <repalasanthoshkumar@gmail.com>
2022-04-14 23:33:40 +0530

    SellingPriceCalculation

---
+++ SellingPriceCalculation.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class SellingPriceCalculation {
+
+/*Participants are expected to run the main method and give input, that will be read by
+  scanner .
+  Avoid making any changes in main method*/
+
+  public static void main(String[] args) {
+    SellingPriceCalculation sellingPriceCalculation = new SellingPriceCalculation();
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter Item name");
+    String productName = scanner.nextLine();
+    System.out.println("Enter Selling Price");
+    float wholeSalePrice = scanner.nextFloat();
+    float retailPrice =  sellingPriceCalculation.revisedSellingPriceCalculator(productName , wholeSalePrice);
+    System.out.println("Revised Selling Price for  " + productName + " is $" + retailPrice );
+  }
+
+
+
+  // Function accepts product name and selling price of item as input
+  // parameters and outputs revised selling price of the item.
+
+
+
+  public float revisedSellingPriceCalculator(String productName, float sellingPrice) {
+       return sellingPrice+sellingPrice*0.05f;
+  }
+
+}
